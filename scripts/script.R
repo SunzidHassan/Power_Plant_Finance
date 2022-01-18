6c4e40998eebce57fdbec9d18532022afc7bf905
Sunzid Hassan <sunzid.hassan@gmail.com>
2022-01-19 03:42:37 +0600

    Accurate LUEC calculation

--- scripts/script.R
+++ scripts/script.R
@@ -1,47 +1,105 @@
+#Read constants and variables of power plant
+
 constants <- read.csv("input/constants.csv",
                                stringsAsFactors = F, header = T, comment.char = "")
-
 
 variables <- read.csv("input/variables.csv",
                       stringsAsFactors = F, header = T, comment.char = "")
 
-n <- 1
-discount_factor <- 0
+##common variables####
 
-while (n < (constants$plant_lifetime+1)) {
-     discount_factor <- discount_factor + (1/(1+constants$discount_rate))^n
-     n <- n+1
+#yearly operation hour
+yearly_operation_hour <- variables$yearly_operation_hour * variables$yearly_operation_days
+
+#discount factor function
+
+discount_factor <- function(time, rate){
+        n <- 1
+        discount <- 1
+        while(n < time+1){
+                discount <- discount + (1/(1+rate)^n)
+                n <- n+1
+        }
+        return(discount)
 }
 
-
-operating_cost <- (constants$annual_electricity_generation * variables$operation_maintenance_cost
-                    * variables$plant_capacity_factor * 10^6)
-
-fuel_cost <- (constants$annual_electricity_generation * variables$fuel_cost
-              * variables$plant_capacity_factor * 10^6)
-
-total_cost <- operating_cost + fuel_cost
-
-
-present_value_electricity <- constants$plant_lifetime * constants$annual_electricity_generation * discount_factor * 1000/24
-
-interest <- constants$investment * constants$interest
-
-yearly_payment <- constants$investment / constants$repayment_period
-
-
-left <- constants$investment
-n <- 1
-totalintPV <- 0
-intPV <- 0
-
-while (n < (constants$repayment_period+1)) {
-     interestVal <- left*constants$interest
-     intPV <- interestVal / ((1+constants$discount_rate) ^ n)
-     totalintPV <- totalintPV + intPV
-     n <- n+1
+#discounted_sum function
+discounted_sum <- function(int_rate, disc_rate, time, starting_amount, step_change){
+        left <- starting_amount
+        n <- 1
+        sum <- 0
+        while(n < (time+1)){
+                val <- left * int_rate
+                sum <- ((val / ((1 + disc_rate) ^ n))
+                        + sum)
+                left <- left - step_change
+                n <- n+1
+        }
+        return(sum)
 }
 
+#plant lifetime discount factor
+plant_lifetime_discount_factor <- discount_factor(constants$plant_lifetime,
+                                                  variables$discount_rate)
 
-LUEC <- ((constants$investment + totalintPV + total_cost + constants$decommissioning_and_waste_cost)
-          / (present_value_electricity * variables$plant_capacity_factor * 100))
+##income####
+#value factor of MWh produced in lifetime
+PV_lifetime_unit_electricity_price_factor <- (constants$plant_capacity * variables$plant_capacity_factor
+                                    * yearly_operation_hour
+                                    * plant_lifetime_discount_factor)
+
+##variable costs####
+
+#lifetime MWh variable cost calculation
+PV_lifetime_unit_operating_cost <- (constants$plant_capacity * variables$plant_capacity_factor
+                   * variables$operation_maintenance_cost
+                   * yearly_operation_hour
+                   * plant_lifetime_discount_factor)
+
+PV_lifetime_unit_fuel_cost <- (constants$plant_capacity * variables$plant_capacity_factor
+              * variables$fuel_cost
+              * yearly_operation_hour
+              * plant_lifetime_discount_factor)
+
+
+PV_lifetime_unit_variable_cost <- (PV_lifetime_unit_operating_cost
+                                   + PV_lifetime_unit_fuel_cost)
+
+
+##cost of capital####
+
+#present value of lifetime capital payment
+
+yearly_capital_payment <- constants$investment / constants$repayment_period
+
+capital_payment_discount_factor <- discount_factor(constants$repayment_period,
+                                                   variables$discount_rate)
+
+PV_lifetime_capital_payment <- yearly_capital_payment * capital_payment_discount_factor
+
+# present value of lifetime interest payment
+
+PV_lifetime_interest_payment <- discounted_sum(constants$interest_rate, variables$discount_rate,
+                                               constants$repayment_period, constants$investment,
+                                               yearly_capital_payment)
+
+#present value of cost of capital
+
+PV_lifetime_costs_of_capital <- PV_lifetime_capital_payment + PV_lifetime_interest_payment
+
+
+
+LUEC <- ((PV_lifetime_costs_of_capital + PV_lifetime_unit_variable_cost + constants$decommissioning_and_waste_cost)
+         / (PV_lifetime_unit_electricity_price_factor))
+
+#the result is $51.16 (BDT 4.34/unit), whereas the official value is $56.73
+
+#Break even
+
+
+#internal rate of return
+
+
+#Weighted Average Cost of Capital
+
+
